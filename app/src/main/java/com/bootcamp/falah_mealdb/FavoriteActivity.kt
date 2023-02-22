927851dcd3cddd3f57162951a3e94ab9bc2162b1
Hafiz37 <hafizfalah93@gmail.com>
2023-02-22 20:00:03 +0700

    view binding main&favorite

--- app/src/main/java/com/bootcamp/falah_mealdb/FavoriteActivity.kt
+++ app/src/main/java/com/bootcamp/falah_mealdb/FavoriteActivity.kt
@@ -2,16 +2,16 @@ package com.bootcamp.falah_mealdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.rawgbootcampidn.databinding.ActivityMainBinding
+import com.example.rawgbootcampidn.databinding.ActivityFavoriteBinding
 
-class MainActivity : AppCompatActivity() {
+class FavoriteActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityFavoriteBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityFavoriteBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
 
